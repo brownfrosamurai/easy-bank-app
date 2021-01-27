931e654020dffa9f29480fd098afefaceabdb834
meduna femi <femimeduna@gmail.com>
2021-01-27 18:33:53 +0100

    mobile navigation menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,25 @@
-console.log('hello world');
+const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+
+btnHamburger.addEventListener('click', function() { // close menu
+    if (header.classList.contains('open')) {
+        body.classList.remove('noscroll');
+        header.classList.remove('open');
+        fadeElems.forEach(function(element) {
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
+
+    } else { // open menu
+        body.classList.add('noscroll');
+        header.classList.add('open');
+        fadeElems.forEach(function(element) {
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        })
+        
+    }
+});
